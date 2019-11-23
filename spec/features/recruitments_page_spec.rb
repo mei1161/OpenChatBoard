303bc22c8a963186e98ff8a894d30392c3e9ee85
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-11-23 13:51:02 +0900

    fix test keywords

--- spec/features/recruitments_page_spec.rb
+++ spec/features/recruitments_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'Recruitments' , js: true do
-  specify 'ShowRecruitments' do
+feature 'Recruitments' , js: true do
+  scenario 'ShowRecruitments' do
     visit '/recruitments'
     page.save_screenshot("ShowRecruitments-#{DateTime.now}.png")
     expect(page).to have_css('p',text: 'Test Room')
