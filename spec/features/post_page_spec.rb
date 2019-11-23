09c06c4a01661b941d989b1f576846892b494e50
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-11-23 13:25:26 +0900

    save a screenshot for test

--- spec/features/post_page_spec.rb
+++ spec/features/post_page_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'Postpage' , js: true do
   specify 'DisplayGreeting' do
     visit '/'
+    page.save_screenshot("DisplayGreeting-#{DateTime.now}.png")
     expect(page).to have_css('p',text: 'Top')
   end
 end
