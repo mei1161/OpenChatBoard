4454e9f77a2f02d643a442e85b2d2ffeef6c6514
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-11-23 14:01:21 +0900

    Add recruitment and listing tests

--- spec/features/recruitments_page_spec.rb
+++ spec/features/recruitments_page_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 feature 'Recruitments' , js: true do
   scenario 'ShowRecruitments' do
+    visit '/recruitments/new'
+    fill_in 'Room Name', with: 'asansroom'
+    click 'Recruit'
+
     visit '/recruitments'
     page.save_screenshot("ShowRecruitments-#{DateTime.now}.png")
-    expect(page).to have_css('p',text: 'Test Room')
+    expect(page).to have_css('p',text: 'asansroom')
   end
 end
