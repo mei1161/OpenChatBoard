50e1be2c0b44b9e8c087a9aca18cf101d6ff7493
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-11-26 13:06:26 +0900

    add show test

---
+++ spec/features/show_page_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'ShowPage' , js: true do
+  scenario 'ShowRoomName' do
+    visit '/recruitments'
+    click_on 'test012'
+
+    visit '/recruitments/34'
+    page.save_screenshot("ShowRoomName-#{DateTime.now}.png")
+    expect(page).to have_css('p',text: 'test012')
+  end
+end
+
