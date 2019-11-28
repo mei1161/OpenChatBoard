3f9a3623b934a39a474d419fbd2e4259e2eea422
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-11-28 11:57:10 +0900

    Add validation to room_url

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,3 +1,11 @@
 class Recruitment < ApplicationRecord
   validates :room_name,presence: true
+  validate :check_url
+
+  private
+   def check_url
+     unless room_url.starts_with? 'https://line.me/ti/g2/'
+       errors.add(:room_url,"Invalid URL")
+     end
+   end
 end
