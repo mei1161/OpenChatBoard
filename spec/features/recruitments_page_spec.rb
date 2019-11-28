e560e67c5b56568f52db60426418f6513b1925db
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-11-28 10:23:39 +0900

    change a test

--- spec/features/recruitments_page_spec.rb
+++ spec/features/recruitments_page_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 feature 'Recruitments' , js: true do
   scenario 'ShowRecruitments' do
     visit '/recruitments/new'
-    fill_in 'Room Name', with: 'asansroom'
-    fill_in 'URL',with: 'aaaa'
+    fill_in 'Room Name', with: 'testchat'
+    fill_in 'URL',with: 'https://line.me/ti/g2/EUz'
     fill_in 'Description',with: 'aa'
     click_on 'Recruit'
 
     visit '/recruitments'
     page.save_screenshot("ShowRecruitments-#{DateTime.now}.png")
-    expect(page).to have_css('p',text: 'asansroom')
+    expect(page).to have_css('p',text: 'testchat')
   end
 end
