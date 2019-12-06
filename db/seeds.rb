81ef3e1bb488d03a65943fabd391f18d69e27f56
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-12-06 15:39:49 +0900

    setup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 # coding: utf-8
 
 
-Recruitment.create :room_name => '集合',
-  :description => '説明',
-  :room_url => 'https://line.me/ti/g2/EUspWaMfsZ0PnuS9dkxI2g',
-  :password_digest => BCrypt::Password.create('abcde')
+recruitment_001 = 
+  Recruitment.create :room_name => '集合',
+    :description => '説明',
+    :room_url => 'https://line.me/ti/g2/EUspWaMfsZ0PnuS9dkxI2g',
+    :password_digest => BCrypt::Password.create('abcde')
+
+recruitment_001.recruitment_comments.create :text => "Hello",
+  :password_digest => BCrypt::Password.create('abcde') 
+
+
 
                    
