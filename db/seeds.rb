6753db797122bcb3e9115be00ec56c52b4e94c3b
mei1161 <ichigo_happy@ezweb.ne.jp>
2019-12-06 15:33:58 +0900

    setup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+
+Recruitment.create :room_name => '集合',
+  :description => '説明',
+  :room_url => 'https://line.me/ti/g2/EUspWaMfsZ0PnuS9dkxI2g',
+  :password_digest => BCrypt::Password.create('abcde')
+
+                   
